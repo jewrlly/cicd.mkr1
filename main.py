164a886886c9a58ceff765bf5cd5c55cbc9e7949
Polina <polinaonipcuk@gmail.com>
2026-04-08 14:07:26 +0300

    add count_words function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 def read_file(filepath):
     with open(filepath, "r", encoding = "utf-8") as f:
         return f.read()
+
+def count_words(text):
+    separators = [",", ":", ";", "\n"]
+    for sep in separators:
+        text = text.replace(sep, " ")
+    words = [w for w in text.split(" ") if w.strip()]
+    return len(words)
